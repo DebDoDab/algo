b6d18b9187e126a39afa3e4d49f9b5cb25fd14db
DebDoDab <debbius3@gmail.com>
2018-10-12 23:40:26 +0300

    Preflow Push

--- preflow_push.cpp
+++ preflow_push.cpp
@@ -4,145 +4,135 @@ typedef signed kashkevich;
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
-typedef double ld;
+typedef long double ld;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
 #define endl "\n"
 #define INF 2000000000
-#define INFLL 1000000000000000000ll
+#define INFLL 7000000000000000000ll
 #define ft first
-#define EPS 0.0001
+#define EPS 0.000001
 #define sd second
 #define pb push_back
 #define faster ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define openfiles ifstream cin("input.txt");ofstream cout("output.txt");
-#define openfiles2 ifstream cin("teleports.in");ofstream cout("teleports.out");
+#define in insert
 
-    int T, n, m;
-    int v;
+int n, s, t, m;
+vector<vector<int>> G, F;
+vector<int> H, E;
+vector<int> MaxH;
 
-
-    set<pair<int, pii>> S;
-    set<int> pos;
-
-    int M(const int &y, const int &x) {
-        return (x - y < 0)? x - y + T : x - y;
-    }
-
-    void Add(int &x) {
-        if (pos.size() == 0) {
-            pos.insert(x);
-            S.insert({0, {x, x}});
-        } else {
-
-            auto ait = upper_bound(pos.begin(), pos.end(), x),
-                 bit = lower_bound(pos.begin(), pos.end(), x);
-            if (ait == pos.end()) {
-                ait = pos.begin();
-            }
-            int a = *ait;
-            if (bit == pos.begin()) {
-                bit = pos.end();
-            }
-            bit--;
-            int b = *bit;
-
-            //if (S.size() != 2)
-            S.erase({M(b, a), {b, a}});
-            S.insert({M(b, x), {b, x}});
-            S.insert({M(x, a), {x, a}});
-
-            pos.insert(x);
-
-            //cout << "ADD " << x << " : " << a << " " << b << endl << endl;
+vector<int> aaa() {
+    vector<int> MX;
+    for (int i = 1; i <= n; i++) {
+        if (E[i] > 0 && !MX.empty() && H[i] > H[MX[0]]) {
+            MX.clear();
+        }
+        if (E[i] > 0 && (MX.empty() || H[i] == H[MX[0]])) {
+            MX.pb(i);
         }
     }
+    return MX;
 
-    void Del(int &x) {
-        if (pos.size() == 1) {
-            S.clear();
-            pos.clear();
-        } else {
+}
 
-            auto ait = upper_bound(pos.begin(), pos.end(), x),
-                 bit = lower_bound(pos.begin(), pos.end(), x);
-            if (ait == pos.end()) {
-                ait = pos.begin();
-            }
-            int a = *ait;
-            if (bit == pos.begin()) {
-                bit = pos.end();
-            }
-            bit--;
-            int b = *bit;
-
-            S.erase({M(b, x), {b, x}});
-            S.erase({M(x, a), {x, a}});
-            S.insert({M(b, a), {b, a}});
-
-            pos.erase(x);
-
-            //cout << "DEL " << x << " : " << a << " " << b << endl << endl;
-        }
-    }
-
-    int Gotcha() {
-        if (S.size() == 0 || S.size() == 1) {
-            return T;
-        }
-        auto ait = S.end(),
-             bit = S.end();
-        bit--;
-        bit--;
-        ait--;
-        //cout << ait -> ft << " " << ait -> sd << endl;
-        //cout << bit -> ft << " " << bit -> sd << endl;
-        //cout << endl;
-
-        return ait->ft +
-                bit->ft;
-    }
-
-kashkevich main(){
+signed main()
+{
     //openfiles;
-    //freopen("teleports.in", "r", stdin);
-    //freopen("teleports.out", "w", stdout);
     //faster;
 
-    //cin >> n >> m >> T >> v;
-    scanf("%d%d%d%d", &n, &m, &T, &v);
+    cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
+    t = 0;
+    s = n+1;
+
+    G.assign(n+2, vector<int>(n+2, 0));
+    F.assign(n+2, vector<int>(n+2, 0));
+    E.assign(n+2, 0);
+    H.assign(n+2, 0);
+    E[0] = INF;
+    H[0] = n+2;
+
+    for (int i = 0; i < m; i++) {
         int x;
-        scanf("%d", &x);
-        Add(x);
-
-        //for (auto x : S) std::cout << x.ft << " " << x.sd << endl;
-        //std::cout << endl;
+        cin >> x;
+        G[0][x] = 1;
+        F[0][x] = G[0][x];
+        F[x][0] = -F[0][x];
+        E[x] = G[0][x];
+    }
+    for (int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+        G[x][s] = 1;
     }
 
-    //cout << fixed << setprecision(7);
-    //cout << Gotcha()/v << endl;
-    printf("%f\n", Gotcha()/(2. * v));
-    //cout << Gotcha()/(2. * v)<< endl;
-    for (int i = 0; i < m; i++) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            char x;
+            cin >> x;
+            if (x == '1')
+                G[i][j] = INF;
+        }
+    }
 
-        char x, levy;
-        int f = 0;
-        scanf("%c%c%d", &x, &levy, &f);
-        if (x == '-') {
-            Del(f);
-        } else {
-            Add(f);
+    while(!(MaxH = aaa()).empty()) {
+
+        if (MaxH.empty()) {
+            break;
         }
 
-        //for (auto x : S) std::cout << x.ft << " " << x.sd << endl;
-        //std::cout << endl;
+        for(int x : MaxH) {
 
-        //cout << Gotcha()/v << endl;
-        printf("%.7f\n", Gotcha()/2./v);
-        //cout << Gotcha()/(2. * v) << endl;
+            bool pushed = 0;
+
+            for (int i = 0; i <= n+1 && E[x]; i++) {
+                if (G[x][i] - F[x][i] > 0 && H[x] == H[i]+1) {
+                    pushed = 1;
+                    int d = min(G[x][i] - F[x][i], E[x]);
+                    F[x][i] += d;
+                    F[i][x] -= d;
+                    E[x] -= d;
+                    E[i] += d;
+                }
+            }
+
+            if (!pushed) {
+
+                int d = INF;
+                for (int i = 0; i <= n+1; i++) {
+                    if (G[x][i] - F[x][i] > 0) {
+                        d = min(d, H[i]);
+                    }
+                }
+                if (d < INF) {
+                    H[x] = d + 1;
+                }
+                break;
+
+            }
+
+        }
+
     }
+
+
+    int flow = 0;
+    for (int j = 0; j <= n+1; j++) {
+
+        if (G[0][j]) {
+            flow += F[0][j];
+        }
+
+    }
+
+    if (flow == m) {
+        cout << "YES\n";
+    } else {
+        cout << "NO";
+    }
+
     return 0;
 }
